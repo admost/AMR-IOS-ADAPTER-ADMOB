11d904c9d189a313751573022eb34b4c8f25c6b7
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2026-03-17 14:36:38 +0000

    Update SPM binary checksum for 13.1.2

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .binaryTarget(
             name: "AMRAdapterAdmob",
             url: "https://github.com/admost/AMR-IOS-ADAPTER-ADMOB/releases/download/13.1.2/AMRAdapterAdmob.xcframework.zip",
-            checksum: "c9fe5cc603d7f245c9e3e01f61e6caedb38820608d4f85cdf71cce3491323f9c"
+            checksum: "6eb9fff6c9ed77460b9fc981a6238db694d7ddabaa12b1ba2d928251d2dcbd82"
         )
     ]
 )
